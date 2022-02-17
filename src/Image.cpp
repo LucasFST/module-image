0aafd8d1090baaa94f1663d1d6bb14fd8857d673
Lucas <lucas.faustmann@gmail.com>
2022-02-17 16:54:04 +0100

    correction oubli

--- src/Image.cpp
+++ src/Image.cpp
@@ -19,12 +19,7 @@ Image::Image(unsigned int dimX, unsigned int dimY)
     assert (dimX<50000 && dimY<50000 ); //taille limitée pour dimX et dimY, pas de verification si negatif puisque ce sont des unsigned int.
     dimx=dimX;
     dimy=dimY;
-    tab=new Pixel[dimX*dimY];
-    // for (unsigned int i=0; i<=dimX*dimY; i++)
-    // {
-    //        tab[i];
-    // }
-   
+    tab=new Pixel[dimX*dimY];   
 }
 
 
@@ -38,8 +33,6 @@ Image::~Image()
 
 Pixel Image::getPix (unsigned int x,unsigned int y) const
     {
-        assert (x<dimx && y<dimy );//taille limitée pour dimx et dimy, pas de verification si negatif puisque ce sont des unsigned int.
-        return tab[y*dimx+x];  //y*dimx+y+x
         assert (x<dimx && y<dimy ); //x et y doivent être respectivement plus petit que dimx et dimy
         return tab[y*dimx+x];  //formule pour passer d'un tab 2D à un tab 1D
 
@@ -49,12 +42,10 @@ Pixel Image::getPix (unsigned int x,unsigned int y) const
 
 void Image::setPix (unsigned int x,unsigned int y, const Pixel& couleur)
     {
-        assert (x<dimx && y<dimy );
         assert (x<dimx && y<dimy ); //x et y doivent être respectivement plus petit que dimx et dimy
         
         tab [y*dimx+x].setRouge(couleur.getRouge());  //on attribue un couleur bien spécifique a notre pixel (R G B)
         tab [y*dimx+x].setVert(couleur.getVert());
-        tab [y*dimx+x].setRouge(couleur.getRouge());
         tab [y*dimx+x].setBleu(couleur.getBleu());
     }
 
@@ -63,12 +54,10 @@ void Image::setPix (unsigned int x,unsigned int y, const Pixel& couleur)
 
 void Image::dessinerRectangle (unsigned int Xmin,unsigned int Ymin,unsigned int Xmax,unsigned int Ymax, const Pixel& couleur)
      {
-        for (unsigned int i=Ymin; i<=Ymax; i++)
         for (unsigned int i=Ymin; i<=Ymax; i++) 
             {
             for (unsigned int j=Xmin; j<=Xmax; j++ )
                 {
-                   setPix(j,i,couleur);
                    setPix(j,i,couleur); //on change la couleur des pixels qui seront dans le rectangle 
                 }
 
@@ -80,18 +69,11 @@ void Image::dessinerRectangle (unsigned int Xmin,unsigned int Ymin,unsigned int 
 
 void Image::effacer (const Pixel& couleur)
     {
-        dessinerRectangle (0,0,dimx-1,dimy-1,couleur);
         dessinerRectangle (0,0,dimx-1,dimy-1,couleur); // on recouvre toute l'image par la couleur en parametre grâce a dessinerRectange 
-
     }
 
 void Image::testRegression ()
 {
-    dimx=2;
-    dimy=3;
-    tab = new Pixel[dimx*dimy];
-    Pixel pxTestNoir = getPix(1,2);
-    assert(pxTestNoir.getVert()==0);
     dimx=2; //valeur choisi arbitrairement 
     dimy=3; //valeur choisi arbitrairement 
 
@@ -100,10 +82,6 @@ void Image::testRegression ()
     assert(pxTestNoir.getVert()==0);//on teste si ce pixel est bien noir (couleur par defaut = 0;0;0)
     assert(pxTestNoir.getRouge()==0);
     assert(pxTestNoir.getBleu()==0);
-    Pixel pxBlanc(255,255,255);
-    dessinerRectangle(0,0,1,1, pxBlanc);
-    Pixel pxTestBlanc = getPix(0,0);
-    assert(pxTestBlanc.getVert()==255);
 
     Pixel pxBlanc(255,255,255); //on creer un pixel blanc 
     dessinerRectangle(0,0,1,1, pxBlanc); // on dessine un rectangle avec ce pixel (en blanc) sur une partie de l'image 
@@ -111,24 +89,17 @@ void Image::testRegression ()
     assert(pxTestBlanc.getVert()==255); // on teste si il est bien blanc (255,255,255)
     assert(pxTestBlanc.getRouge()==255);
     assert(pxTestBlanc.getBleu()==255);
-    assert(pxTestNoir.getVert()==0);
 
     //on teste si le pixel noir l'est toujours (il n'a pas été recouvert par le rectangle)
     assert(pxTestNoir.getVert()==0); 
     assert(pxTestNoir.getRouge()==0);
     assert(pxTestNoir.getBleu()==0);
-    effacer(pxBlanc);
-    pxTestNoir = getPix(1,2);
 
     effacer(pxBlanc); // rempli l'image par un rectangle entierement blanc 
     pxTestNoir = getPix(1,2); // on prend le pixel anciennement noir et on teste si il est blanc (255,255,255)
     assert(pxTestNoir.getVert()==255);
     assert(pxTestNoir.getRouge()==255);
     assert(pxTestNoir.getBleu()==255);
-    Pixel pxCouleur(145,167,23);
-    setPix(1,1,pxCouleur);
-    Pixel Test = getPix(1,1);
-    assert(Test.getRouge()==145);
 
     Pixel pxCouleur(145,167,23); //on creer un pixel(pxCouleur) d'une couleur arbitraire
     setPix(1,1,pxCouleur); //on change un pixel blanc par le pxCouleur 
