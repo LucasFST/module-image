ddd7010240b21548a27b2ec94fd00a7db28a33e2
Lucas <lucas.faustmann@gmail.com>
2022-02-17 16:42:20 +0100

    zebi

--- src/mainExemple.cpp
+++ src/mainExemple.cpp
@@ -13,13 +13,13 @@ int main() {
     image1.dessinerRectangle(10, 40, 20, 44, blanc);
     image1.setPix(2,9,bleu);
     image1.setPix(15,42,bleu);
-    image1.sauver("./data/image1.ppm");
+    image1.sauver("../data/image1.ppm");
  
     Image image2;
-    image2.ouvrir("./data/image1.ppm");
+    image2.ouvrir("../data/image1.ppm");
     image2.dessinerRectangle(4, 14, 20, 43, blanc);
     image2.dessinerRectangle(32, 4, 50, 8, bleu);
-    image2.sauver("./data/image2.ppm");
+    image2.sauver("../data/image2.ppm");
  
     return 0;
 }
