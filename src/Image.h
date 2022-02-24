a22e6b7ecdac4e658a8f332d988c5adeb74528e3
Lucas <lucas.faustmann@gmail.com>
2022-02-24 15:14:58 +0100

    MAJ doxy

--- src/Image.h
+++ src/Image.h
@@ -10,24 +10,30 @@ using namespace std;
 */
 class Image {
 
-    private:
-        unsigned int dimx,dimy;
-        Pixel* tab;
-
     public:
       
         /**
         * @brief Constructeur par défaut de la classe : initialise dimx et dimy à 0, ce constructeur n'alloue pas de pixel
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * Image une_Image;
+        * @endcode
         */
         Image ();
 
     
 
         /**
-        *   @brief  Constructeur de la classe : initialise dimx et dimy (après vérification), puis alloue le tableau de pixel dans le tas (image noire)
-        *   @param[in] dimx la dimension en abcsisse de notre image 
-        *   @param[in] dimy la dimension en ordonnée de notre image 
-        *   @remark dimx et dimy ne peuvent être supérieurs ou égaux à 50000 (limite arbitraire)
+        * @brief  Constructeur de la classe : initialise dimx et dimy (après vérification), puis alloue le tableau de pixel dans le tas (image noire)
+        * @param[in] dimx la dimension en abcsisse de notre image 
+        * @param[in] dimy la dimension en ordonnée de notre image 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * Image une_Image(2,3);
+        * @endcode
+        * @warning dimx et dimy ne peuvent être supérieurs ou égaux à 50000 (limite arbitraire)
         */
         Image (unsigned int dimx, unsigned int dimy);
 
@@ -35,89 +41,142 @@ class Image {
 
       
         /**
-        *   @brief  Destructeur de la classe : désallocation de la mémoire du tableau de pixel et mise à jour des champs dimx et dimy à 0
+        * @brief  Destructeur de la classe : désallocation de la mémoire du tableau de pixel et mise à jour des champs dimx et dimy à 0
         */
         ~Image ();
 
 
 
         /**
-        *   @brief   Accesseur : récupère le pixel original de coordonnées (x,y) en vérifiant leur validité
-        *   @param[in] x la coordonnée x du pixel
-        *   @param[in] y la coordonnée y du pixel
-        *   @return Pixel 
-        *   @remark x et y doivent être inférieurs strictement à dimx et dimy respectivement (dimensions de l'image) 
+        * @brief   Accesseur : récupère le pixel original de coordonnées (x,y) en vérifiant leur validité
+        * @param[in] x la coordonnée x du pixel
+        * @param[in] y la coordonnée y du pixel
+        * @return Pixel 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * Pixel un_Pixel = une_Image.getPix(1,1)
+        * @endcode
+        * @warning x et y doivent être inférieurs strictement à dimx et dimy respectivement (dimensions de l'image) 
         */
         Pixel getPix (unsigned int x, unsigned int y) const; //retourne Pixel ou Pixel&
 
 
       
         /**
-        *   @brief   Mutateur : modifie la couleur du pixel de coordonnées (x,y) en vérifiant leur validité
-        *   @param[in] x la coordonnée x du pixel
-        *   @param[in] y la coordonnée y du pixel
-        *   @param[in,out] couleur un pixel de la nouvelle couleur désirée (attention : en mode donnée/résultat mais CONST)
-        *   @return void 
-        *   @remark x et y doivent être inférieurs strictement à dimx et dimy respectivement (dimensions de l'image)
+        * @brief   Mutateur : modifie la couleur du pixel de coordonnées (x,y) en vérifiant leur validité
+        * @param[in] x la coordonnée x du pixel
+        * @param[in] y la coordonnée y du pixel
+        * @param[in,out] couleur un pixel de la nouvelle couleur désirée (attention : en mode donnée/résultat mais CONST)
+        * @return void
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * Pixel Blanc(255,255,255);
+        * une_Image.setPix(1,1,Blanc);
+        * @endcode 
+        * @warning x et y doivent être inférieurs strictement à dimx et dimy respectivement (dimensions de l'image)
         */
         void setPix (unsigned int x, unsigned int y, const Pixel& couleur );
 
 
         /**
-        *   @brief   Dessine un rectangle plein de la couleur désirée dans l'image 
-        *   @param[in] Xmin la composante x du coin haut gauche  
-        *   @param[in] Ymin la composante y du coin haut gauche 
-        *   @param[in] Xmax la composante x du coin bas droite
-        *   @param[in] Ymax la composante x du coin bas droite 
-        *   @param[in,out] couleur un pixel de la couleur du rectangle désiré (attention : en mode donnée/résultat mais CONST)
-        *   @return void 
+        * @brief   Dessine un rectangle plein de la couleur désirée dans l'image 
+        * @param[in] Xmin la composante x du coin haut gauche  
+        * @param[in] Ymin la composante y du coin haut gauche 
+        * @param[in] Xmax la composante x du coin bas droite
+        * @param[in] Ymax la composante x du coin bas droite 
+        * @param[in,out] couleur un pixel de la couleur du rectangle désiré (attention : en mode donnée/résultat mais CONST)
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * Pixel Blanc(255,255,255);
+        * une_Image.dessinerRectangle(0,0,2,3,Blanc);
+        * @endcode
         */
         void dessinerRectangle (unsigned int Xmin, unsigned int Ymin, unsigned int Xmax, unsigned int Ymax, const Pixel& couleur );
 
         
         /**
-        *   @brief Efface l'image en la remplissant de la couleur passée en paramètre 
-        *   @param[in,out] couleur un pixel de la couleur désirée (attention : en mode donnée/résultat mais CONST) 
-        *   @return void 
+        * @brief Efface l'image en la remplissant de la couleur passée en paramètre 
+        * @param[in,out] couleur un pixel de la couleur désirée (attention : en mode donnée/résultat mais CONST) 
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * Pixel Blanc(255,255,255);
+        * une_Image.effacer(Blanc);
+        * @endcode
         */
         void effacer (const Pixel& couleur);
 
         
         /**
-        *   @brief  Effectue une série de tests vérifiant que le module fonctionne et que les données membres de l'objet sont conformes
-        *   @return void 
+        * @brief  Effectue une série de tests vérifiant que le module fonctionne et que les données membres de l'objet sont conformes
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * une_Image.testRegression();
+        * @endcode
         */
-        void testRegression ();
+        void testRegression ();  //Pertinent ou pas de documenter cette fonction ????????????
 
         
         /**
-        *   @brief Sauver une image dans un fichier
-        *   @param[in,out] filename nom de l'image à sauvegarder (attention : en mode donnée/résultat mais CONST)
-        *   @return void 
+        * @brief Sauver une image dans un fichier
+        * @param[in,out] filename nom de l'image à sauvegarder (attention : en mode donnée/résultat mais CONST)
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * une_Image.sauver("../data/image1.ppm");
+        * @endcode
         */
         void sauver(const string& filename) const;
 
 
         /**
-        *   @brief Ouvrir une image depuis un fichier
-        *   @param[in,out] filename chemin/nom de l'image à ouvrir (attention : en mode donnée/résultat mais CONST)
-        *   @return void 
+        * @brief Ouvrir une image depuis un fichier
+        * @param[in,out] filename chemin/nom de l'image à ouvrir (attention : en mode donnée/résultat mais CONST)
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * une_Image.ouvrir("../data/image1.ppm");
+        * @endcode
         */
         void ouvrir(const string& filename);
 
 
         /**
-        *   @brief Afficher les composantes RGB des pixels sur la console
-        *   @return void 
+        * @brief Afficher les composantes RGB des pixels sur la console
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * une_Image.afficherConsole();
+        * @endcode
         */
         void afficherConsole();
 
 
         /**
-        *   @brief Affiche l'image dans une fenêtre SDL2
-        *   @return void 
+        * @brief Affiche l'image dans une fenêtre SDL2
+        * @return void 
+        * 
+        * Exemple d'utilisation :
+        * @code
+        * une_Image.afficher();
+        * @endcode
         */
         void afficher() const;
+
+    private:
+
+        unsigned int dimx,dimy;
+        Pixel* tab;
 
 };
 
--- src/Pixel.h
+++ src/Pixel.h
@@ -14,37 +14,62 @@ class Pixel
 
     /**
      * @brief   Constructeur par défaut de la classe : initialise le pixel à la couleur noire
+     * 
+     * Exemple d'utilisation : 
+     * @code
+     * Pixel un_Pixel;
+     * @endcode
     */
 
    Pixel ();
 
     /**
-     * @brief   Constructeur de la classe : initialise r,g,b avec les paramètres
+     * @brief   Constructeur de la classe : initialise r,g,b avec respectivement les paramètres nr,ng,nb
      * @param[in] nr un entier strictement positif représentant la composante rouge du pixel
      * @param[in] ng un entier strictement positif représentant la composante verte du pixel
      * @param[in] nb un entier strictement positif représentant la composante bleue du pixel
-     * @remark nr, ng et nb doivent être inférieurs ou égaux à 255 (unsigned char)
+     * 
+     * Exemple d'utilisation : 
+     * @code
+     * Pixel blanc (255,255,255);
+     * @endcode
+     * @warning nr, ng et nb doivent être inférieurs ou égaux à 255 (unsigned char)
     */
 
    Pixel (unsigned char nr, unsigned char ng, unsigned char nb);
 
     /**
      * @brief   Accesseur : récupère la composante rouge du pixel
-     * @return unsigned char (entier compris entre 0 et 255)     
+     * @return unsigned char (entier compris entre 0 et 255)
+     * 
+     * Exemple d'utilisation : 
+     * @code
+     * unsigned char nr = un_Pixel.getRouge();
+     * @endcode    
     */
 
    unsigned char getRouge () const;
 
     /**
      * @brief   Accesseur : récupère la composante verte du pixel
-     * @return unsigned char (entier compris entre 0 et 255)       
+     * @return unsigned char (entier compris entre 0 et 255)
+     * 
+     * Exemple d'utilisation :
+     * @code
+     * unsigned char ng = un_Pixel.getVert();
+     * @endcode        
     */
 
    unsigned char getVert () const;
 
     /**
      * @brief   Accesseur : récupère la composante bleue du pixel
-     * @return unsigned char (entier compris entre 0 et 255)       
+     * @return unsigned char (entier compris entre 0 et 255) 
+     * 
+     * Exemple d'utilisation :
+     * @code
+     * unsigned char nb = un_Pixel.getBleu();
+     * @endcode       
     */
 
    unsigned char getBleu () const;
@@ -52,8 +77,13 @@ class Pixel
     /**
      * @brief  Mutateur : modifie la composante rouge du pixel
      * @param[in] nr un entier strictement positif représentant la composante rouge du pixel
-     * @return void     
-     * @remark nr doit être inférieur ou égal à 255 (unsigned char)
+     * @return void 
+     * 
+     * Exemple d'utilisation :
+     * @code
+     * un_Pixel.setRouge(148);
+     * @endcode    
+     * @warning nr doit être inférieur ou égal à 255 (unsigned char)
     */
 
    void setRouge (unsigned char nr);
@@ -61,8 +91,13 @@ class Pixel
     /**
      * @brief  Mutateur : modifie la composante verte du pixel
      * @param[in] ng un entier strictement positif représentant la composante verte du pixel 
-     * @return void   
-    * @remark ng doit être inférieur ou égal à 255 (usigned char)
+     * @return void 
+     * 
+     * Exemple d'utilisation :
+     * @code
+     * un_Pixel.setVert(28);
+     * @endcode   
+     * @warning ng doit être inférieur ou égal à 255 (usigned char)
     */
 
    void setVert (unsigned char ng);
@@ -70,8 +105,13 @@ class Pixel
     /**
      * @brief  Mutateur : modifie la composante bleue du pixel
      * @param[in] nb un entier strictement positif représentant la composante bleue du pixel
-     * @return void     
-     * @remark nb doit être inférieur ou égal à 255 (usigned char)
+     * @return void 
+     * 
+     * Exemple d'utilisation :
+     * @code
+     * un_Pixel.setBleu(213);
+     * @endcode     
+     * @warning nb doit être inférieur ou égal à 255 (usigned char)
     */   
 
    void setBleu (unsigned char nb);
